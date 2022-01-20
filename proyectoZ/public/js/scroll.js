9deae7dec1b31c99dcded551701946574ab854b3
CrtNeo <CrtNeo@ubuntu2004.linuxvmimages.local>
2022-01-20 16:37:06 -0500

    Pagina de inicio casi acabada

--- proyectoZ/public/js/scroll.js
+++ proyectoZ/public/js/scroll.js
@@ -1,10 +1,36 @@
-window.addEventListener("scroll", revelarCards);
+window.addEventListener("scroll", fundidoAbajo);
+window.addEventListener("scroll", fundidoIzquierda);
+window.addEventListener("scroll", fundidoDerecha);
 
-function revelarCards(){
+function fundidoAbajo(){
     let cards = document.querySelectorAll('.fundido');
     cards.forEach((card) => {
         var distanciaArriba = card.getBoundingClientRect().top;
         if(distanciaArriba < window.innerHeight - 150){
+            card.classList.add('active');     
+        }else{
+            card.classList.remove('active');     
+        };
+    });
+}
+
+function fundidoIzquierda(){
+    let cards = document.querySelectorAll('.fundido-left');
+    cards.forEach((card) => {
+        var distanciaArriba = card.getBoundingClientRect().top;
+        if(distanciaArriba < window.innerHeight - 400){
+            card.classList.add('active');     
+        }else{
+            card.classList.remove('active');     
+        };
+    });
+}
+
+function fundidoDerecha(){
+    let cards = document.querySelectorAll('.fundido-right');
+    cards.forEach((card) => {
+        var distanciaArriba = card.getBoundingClientRect().top;
+        if(distanciaArriba < window.innerHeight - 400){
             card.classList.add('active');     
         }else{
             card.classList.remove('active');     
