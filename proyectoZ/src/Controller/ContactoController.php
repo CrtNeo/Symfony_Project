b15c54614b55620b3dfcacbcedf1e340c95c20df
CrtNeo <CrtNeo@ubuntu2004.linuxvmimages.local>
2022-01-31 12:33:13 -0500

    En proceso: Formulario

--- proyectoZ/src/Controller/ContactoController.php
+++ proyectoZ/src/Controller/ContactoController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Mensaje;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Form\MensajeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactoController extends AbstractController
@@ -17,4 +21,29 @@ class ContactoController extends AbstractController
             'controller_name' => 'ContactoController',
         ]);
     }
+
+    /*
+    public function enviarMensaje(Request $request)
+    {
+        $mensaje = new Mensaje();
+        $form = $this->createForm(MensajeType::class, $mensaje);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($mensaje);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('contacto/contact.html.twig');
+        }
+
+        return $this->render(
+            'contacto/contact.html.twig',
+            array('form' => $form)
+        );
+        
+    }
+    */
+
 }
