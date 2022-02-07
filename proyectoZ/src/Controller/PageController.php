f921b46878ca7af6378e464293d9cc0b8578ca17
CrtNeo <cesarcris02@gmail.com>
2022-02-07 12:15:16 +0100

    Codigo optimizado

--- proyectoZ/src/Controller/PageController.php
+++ proyectoZ/src/Controller/PageController.php
@@ -2,10 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Marcas;
-use App\Entity\Piezas;
-use App\Entity\Vehiculos;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,33 +20,4 @@ class PageController extends AbstractController
         ]);
     }
 
-    /**
- * @Route("/categoria/{codigo<\d+>?1}", name="autos")
- */
-public function ficha(ManagerRegistry $doctrine, $codigo): Response
-{
-
-    $repositorio = $doctrine->getRepository(Vehiculos::class);
-
-    $vehiculo =  $repositorio->find($codigo);
-
-    return $this->render('vehiculos.html.twig', [
-
-        'vehiculo' => $vehiculo
-
-    ]);
-}
-
-/**
- * @Route("/categoria/buscar/{texto}", name="buscar_auto")
- */
-
-public function buscar(ManagerRegistry $doctrine, $texto): Response
-{
-    $repositorio = $doctrine->getRepository(Vehiculos::class);
-
-    $vehiculos =  $repositorio->findByName($texto);
-
-    return $this->render("lista_autos.html.twig", ['vehiculos' => $vehiculos]);
-}
 }
---
+++ proyectoZ/src/Controller/VehiculosController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehiculos;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehiculosController extends AbstractController
+{
+/**
+     * @Route("/categoria/coches", name="pagina_coches")
+     */
+    public function inicio_coches(ManagerRegistry $doctrine): Response{
+  
+        $repositorio = $doctrine->getRepository(Vehiculos::class);
+
+        $vehiculos =  $repositorio->findBy(["tipos" => "1"]);
+
+        return $this->render('autos/index.html.twig', [
+            'controller_name' => 'AutosController',
+            'vehiculos' => $vehiculos,
+ 
+        ]);
+    }
+
+    /**
+     * @Route("/categoria/motos", name="pagina_motos")
+     */
+    public function inicio_motos(ManagerRegistry $doctrine): Response
+    {
+        $repositorio = $doctrine->getRepository(Vehiculos::class);
+
+        $vehiculos =  $repositorio->findBy(["tipos" => "2"]);
+
+        return $this->render('motos/index.html.twig', [
+            'controller_name' => 'MotosController',
+            'vehiculos' => $vehiculos
+        ]);
+    }
+
+        /**
+     * @Route("/categoria/otros", name="pagina_otros")
+     */
+    public function inicio_otros(ManagerRegistry $doctrine): Response
+    {
+        $repositorio = $doctrine->getRepository(Vehiculos::class);
+
+        $vehiculos =  $repositorio->findBy(["tipos" => "3"]);
+
+        return $this->render('otros/index.html.twig', [
+            'controller_name' => 'OtrosController',
+            'vehiculos' => $vehiculos
+        ]);
+    }
+
+      /**
+ * @Route("/categoria/{codigo<\d+>?1}", name="ficha_vehiculo")
+ */
+public function ficha(ManagerRegistry $doctrine, $codigo): Response
+{
+
+    $repositorio = $doctrine->getRepository(Vehiculos::class);
+
+    $vehiculo =  $repositorio->find($codigo);
+
+    return $this->render('vehiculos.html.twig', [
+
+        'vehiculo' => $vehiculo
+
+    ]);
+}
+
+/**
+ * @Route("/categoria/buscar/{texto}", name="buscar_vehiculo")
+ */
+
+public function buscar(ManagerRegistry $doctrine, $texto): Response
+{
+    $repositorio = $doctrine->getRepository(Vehiculos::class);
+
+    $vehiculos =  $repositorio->findByName($texto);
+
+    return $this->render("lista_autos.html.twig", ['vehiculos' => $vehiculos]);
+}
+
+/**
+ * @Route("/categoria/añadir/{tipo}/{nombre}/{pieza}", name="añadir_vehiculo")
+ */
+
+public function addVehiculo(ManagerRegistry $doctrine, $tipo, $nombre, $pieza): Response
+{
+    $entityManager = $doctrine->getManager();
+
+    $vehiculo = new Vehiculos();
+
+    $vehiculo->setTipos($tipo);
+        
+    $vehiculo->setNombre($nombre);
+
+    $vehiculo->addPieza($pieza);
+    
+    $entityManager->persist($vehiculo);    
+
+    try{
+        $entityManager->flush();
+        return new Response("Vehiculo añadido correctamente!");
+    }catch(\Exception $e){
+        throw new \Exception ($e->getMessage(), "\n");
+    }
+}
+}
