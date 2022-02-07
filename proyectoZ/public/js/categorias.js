2cf34d6f2b053f4b23ec54b148f08393a975de1d
CrtNeo <CrtNeo@ubuntu2004.linuxvmimages.local>
2022-02-07 15:32:26 -0500

    Añadir en proceso

---
+++ proyectoZ/public/js/categorias.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    //BUSCADOR
+    $(".boton-buscar").click(function(){
+        var  valorInput = $(".input-search").val();
+        $("form").attr("action", "/categoria/buscar/" + valorInput);
+    })
+
+    //DESPLEGAR AÑADIR
+    $(".icono").click(() =>{
+        $(".add").animate({
+            opacity: 1,
+            height: 'show'
+        }, 450);
+    });
+    $(".cerrar").click(() =>{
+        $(".formulario").trigger( "reset" );
+        $(".add").animate({
+            opacity: 0,
+            height: 'hide'
+        }, 450);
+    });
+    
+});
--- proyectoZ/public/js/search.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $(".boton-buscar").click(function(){
-        var  valorInput = $(".input-search").val();
-        $("form").attr("action", "/categoria/buscar/" + valorInput);
-    })
-});
