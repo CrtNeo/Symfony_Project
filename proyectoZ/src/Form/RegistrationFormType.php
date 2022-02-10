526630b6eda83e5217fe035d47f7034a6ac14241
CrtNeo <cesarcris02@gmail.com>
2022-02-10 12:08:14 +0100

    Register arreglado, hacer que se vea el usuario

--- proyectoZ/src/Form/RegistrationFormType.php
+++ proyectoZ/src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -32,6 +33,7 @@ class RegistrationFormType extends AbstractType
                 ],
                 
             ])
+            ->add('guardar', SubmitType::class)
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
